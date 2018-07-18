d0555a7eca254ac23014a1895d5838c08eacf1b2
Pavel <pvdymov@ya.ru>
2018-07-18 18:49:54 +0700

    + комментарий к task1

--- src/main/java/ru/javacore/lesson4/App.java
+++ src/main/java/ru/javacore/lesson4/App.java
@@ -11,6 +11,8 @@ import ru.javacore.lesson4.task1.PrintString;
 public class App {
 
     public static void main(String[] args) {
+        // 1. Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз (порядок
+        //– ABСABСABС). Используйте wait/notify/notifyAll.
         Controller controller = new Controller(3);
         new PrintString(controller, "A", 1);
         new PrintString(controller, "B", 2);
